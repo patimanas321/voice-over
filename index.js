7ddc3a99777bff3316808c11526d2ebfbffbfe19
Manas <patimanas321@yahoo.com>
2018-11-23 18:46:19 +0530

    enabling CORS

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const configHelper = require('./helpers/config-helper');
 const userRoute = require('./routers/user');
 
 app.use(bodyParser.json());
+app.use(cors({
+    origin: configHelper.getConfigValue('allowedOrigins')
+}));
+
 app.use('/user', userRoute);
 
 const port = configHelper.getConfigValue('port');
